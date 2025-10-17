f1c0362b77edb780c509c2b3fb4a949ad9f5efd7
alonrav74-sys <alonrav74@gmail.com>
2025-10-17 16:43:17 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('chordai-v1').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icons/icon-192.png',
+        './icons/maskable-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
