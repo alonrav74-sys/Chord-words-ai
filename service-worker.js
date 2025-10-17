3486974bcc4b0d8fe9e292b61602e74851e0f510
alonrav74-sys <alonrav74@gmail.com>
2025-10-18 00:58:50 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,11 @@
 
-const CACHE = "cfai-947";
+const CACHE = "cfai-947-full";
+const CORE = ["./","./index.html","./manifest.json","./models/whisper-tiny.bin","./models/whisper-tiny.js"];
 self.addEventListener("install", e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"])));
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE.map(u=>new Request(u,{cache:'reload'}))).catch(()=>{})));
+});
+self.addEventListener("activate", e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
 });
 self.addEventListener("fetch", e=>{
   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
