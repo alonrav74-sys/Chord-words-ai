292c983d38aa71529226e660e89dd1d0dede8f1c
alonrav74-sys <alonrav74@gmail.com>
2025-10-17 20:53:34 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,6 @@
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('chordai-v1').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './icons/icon-192.png',
-        './icons/maskable-512.png'
-      ]);
-    })
-  );
+  e.waitUntil(caches.open('chordai-v1').then(cache => cache.addAll(['./','./index.html'])));
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
 });
