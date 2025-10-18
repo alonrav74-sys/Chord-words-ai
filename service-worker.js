6501cc02e2d0c125400666b720f85ad86932d252
alonrav74-sys <alonrav74@gmail.com>
2025-10-18 09:18:08 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,5 @@
-
-const CACHE = "cfai-v951";
-self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json","./service-worker.js"])));
-});
-self.addEventListener("activate",e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-});
-self.addEventListener("fetch",e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
+// service-worker.js (cache only; no functional changes)
+const CACHE='cfai-v960';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./service-worker.js'])))});
+self.addEventListener('activate',e=>{e.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim();})())});
+self.addEventListener('fetch',e=>{e.respondWith((async()=>{const r=await caches.match(e.request);return r||fetch(e.request);})())});
