ba56b5b814406af00b43a29a8d455e91485fdcca
alonrav74-sys <alonrav74@gmail.com>
2025-10-18 10:04:52 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,7 @@
 // service-worker.js
-const CACHE='cfai-v946-whisper';
+const CACHE='cfai-v946-whisper-debug';
 const CORE=['./','./index.html','./manifest.json','./service-worker.js',
-  './models/whisper-tiny.js','./models/whisper-tiny.wasm','./models/whisper-tiny.worker.js',
-  './models/ggml-tiny.bin'
+  './models/whisper-tiny.js','./models/whisper-tiny.wasm','./models/whisper-tiny.worker.js','./models/ggml-tiny.bin'
 ];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE).catch(()=>{})))});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{const keys=await caches.keys();await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));await self.clients.claim();})())});
